f32130292c0d69dff790ee1d95adc8778ce70f85
Ifubaraboye <ifubaraboyeoribime@outlook.com>
2025-01-03 00:52:37 +0100

    added file

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+export const Navbar = () => {
+  return (
+    <div className='flex justify-between px-9 px-auto py-4 items-center'>
+        <div className='flex items-baseline'>
+            <p className='text-[25px] font-bold'>Chops</p>
+            <p className='text-[25px] font-bold text-yellow-400'>.</p>
+        </div>
+        
+        <div>
+            <ul className='flex gap-x-14'>
+                <li className=''><Link to='/'>Home</Link></li>
+                <li className='text-gray-500'><Link to='/gallery'>Gallery</Link></li>
+                <li className='text-gray-500'><Link to='/about'>About</Link></li>
+                <li className='text-gray-500'><Link to='/contact'>Contact</Link></li>
+            </ul>
+        </div>
+        <button className='border p-3 rounded-md bg-yellow-400 outline-none text-white font-bold'>Place an Order</button>
+    </div>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,10 @@
+import React from "react";
+import { Navbar } from "../components/Navbar";
+
 export function Home() {
     return (
       <div>
+        <Navbar />
         <h1>Home</h1>
       </div>
     );
