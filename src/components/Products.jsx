e4b928fd9d9bbf039ed198039cbe7dfac92e276e
Ifubaraboye <ifubaraboyeoribime@outlook.com>
2025-01-05 22:07:22 +0100

    modified file

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,14 +1,51 @@
 import React from 'react'
 
-const Products = [{
-    id: 1,
-    category: 'Pack',
-    price: 2000,
-    contents : 'This is a pack of 6 pieces of meat pie',
-    image: 'https://res.cloudinary.com/djvjxp',
-    title: 'Meat Pie'
+const Products = [
+    {
+        id: 1,
+        category: 'Pack',
+        price: 2000,
+        contents : 'This is a pack of 6 pieces of meat pie',
+        image: 'https://res.cloudinary.com/djvjxp',
+        title: 'Meat Pie'
+    },
 
-}]
+    {
+        id: 1,
+        category: 'Pack',
+        price: 2000,
+        contents : 'This is a pack of 6 pieces of meat pie',
+        image: 'https://res.cloudinary.com/djvjxp',
+        title: 'Meat Pie'
+    },
+
+    {
+        id: 1,
+        category: 'Pack',
+        price: 2000,
+        contents : 'This is a pack of 6 pieces of meat pie',
+        image: 'https://res.cloudinary.com/djvjxp',
+        title: 'Meat Pie'
+    },
+
+    {
+        id: 1,
+        category: 'Pack',
+        price: 2000,
+        contents : 'This is a pack of 6 pieces of meat pie',
+        image: 'https://res.cloudinary.com/djvjxp',
+        title: 'Meat Pie'
+    },
+    
+    {
+        id: 1,
+        category: 'Pack',
+        price: 2000,
+        contents : 'This is a pack of 6 pieces of meat pie',
+        image: 'https://res.cloudinary.com/djvjxp',
+        title: 'Meat Pie'
+    }
+]
 
 
 export default Products
