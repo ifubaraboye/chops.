3e8186db8bb4ec864eb4b2d9dc2a7bbf8a5dbb0d
Ifubaraboye <ifubaraboyeoribime@outlook.com>
2025-01-12 21:57:03 +0100

    modified file

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,15 +10,15 @@ export function About() {
         <div className="px-10">
           <p>
 
- <p className="text-[40px] font-bold">Chops: Indulge Your Cravings <br></br></p>
+<p className="text-[40px] font-bold">Chops: Indulge Your Cravings <br></br></p>
 
- <p>At Chops, we're dedicated to bringing you the finest selection of snackables. We believe that life is too short for bland meals, and that every bite should be an explosion of flavor. <br></br></p>
+<p className="text-gray-500 font-medium">At Chops, we're dedicated to bringing you the finest selection of snackables. We believe that life is too short for bland meals, and that every bite should be an explosion of flavor. <br></br></p>
 
-Our Mission: <br></br>
+<p className="text-[40px] font-bold">Our Mission <br></br> </p>
 
-Our mission is to be your go-to destination for all things delicious and meaty. We strive to:
+ <p className="text-gray-500 font-medium">Our mission is to be your go-to destination for all things delicious and meaty. We strive to:</p>
 
-Source the highest quality ingredients: We carefully select premium meats, fresh produce, and the finest spices to ensure that every dish is crafted with excellence. <br></br>
+ <p className="text-gray-500 font-medium">Source the highest quality ingredients: We carefully select premium meats, fresh produce, and the finest spices to ensure that every dish is crafted with excellence. <br></br></p>
 Offer a diverse menu: From succulent steaks and juicy burgers to flavorful curries and savory kebabs, we cater to a wide range of palates with our diverse menu. <br></br>
 Provide a seamless ordering experience: Our user-friendly website and reliable delivery services make it easy to satisfy your cravings whenever they strike. <br></br>
 Promote culinary exploration: We encourage you to step outside your comfort zone and discover new and exciting dishes. <br></br>
